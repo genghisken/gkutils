0c42bf1b8387db52547dbaaadc7ccd9c9a91f0f4
Ken Smith <k.w.smith@qub.ac.uk>
2021-07-29 15:57:04 +0100

    Added memory-lite code to select subset of columns from a csv file.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='A collection useful utilities - mostly related to astronomy',
     long_description=readme(),
     long_description_content_type="text/markdown",
-    version="0.2.3",
+    version="0.2.4",
     author='genghisken',
     author_email='ken.w.smith@gmail.com',
     license='MIT',
@@ -33,6 +33,6 @@ setup(
       ],
     python_requires='>=3.6',
     entry_points = {
-        'console_scripts': ['bruteForceConeSearchATLAS=gkutils.commonutils.bruteForceConeSearchATLAS:main','coneSearchCassandra=gkutils.commonutils.coneSearchCassandra:main'],
+        'console_scripts': ['bruteForceConeSearchATLAS=gkutils.commonutils.bruteForceConeSearchATLAS:main','coneSearchCassandra=gkutils.commonutils.coneSearchCassandra:main','getCSVColumnSubset=gkutils.commonutils.getCSVColumnSubset:main'],
     },
 )
